0be22c68c4c2c67f0b9923b0bb48f00c90c8414c
Andre Barnard <andrebarn@gmail.com>
2017-02-05 09:48:53 +0200

    Adding files

---
+++ HitCounter/HitCounterHub.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace HitCounter
+{
+    [HubName("hitCounter")]
+    public class HitCounterHub : Hub
+    {
+        //dont want to use stacic var in web code, but you knew that
+        static int _hitCount = 0;
+
+        public void RecordHit()
+        {
+            _hitCount++;
+
+            Clients.All.onRecodHit(_hitCount);
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            _hitCount--;
+
+            Clients.All.onRecodHit(_hitCount);
+            return base.OnDisconnected(stopCalled);
+        }
+
+    }
+}
